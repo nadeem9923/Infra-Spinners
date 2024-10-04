ab1040b2c6bb7a13251d169a2e201802ca64a656
root <root@ip-172-31-25-42.ap-southeast-1.compute.internal>
2024-10-04 10:00:18 +0000

    04102024

---
+++ Online-voting-system-project-in-php-master/connection.php
@@ -0,0 +1,3 @@
+<?php 
+$con = mysqli_connect("localhost","root","admin","polltest") or die ("error" . mysqli_error($con));
+?>
---
+++ Online-voting-system-project-in-php-master/footer.php
@@ -0,0 +1,7 @@
+<br>
+<br>
+<br>
+<br>
+<center><font color="red"><b><i>Declaration: </i></b></font><i>DEMO Project&nbsp;</i></center>
+</body>
+</html>
